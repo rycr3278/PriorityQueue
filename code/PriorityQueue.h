37fcb4b58b44060c3defba054907b1426b7fb91a
Ryan <rycr3278@colorado.edu>
2023-07-13 21:34:16 +0000

    almost done

--- code/PriorityQueue.h
+++ code/PriorityQueue.h
@@ -3,6 +3,7 @@
 
 #include <memory>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -11,6 +12,8 @@ using namespace std;
 // them in pq.cpp.
 struct pq {
     // First implement this struct
+    float priority;
+    string text;
 };
 
 
@@ -25,26 +28,28 @@ public:
     // init_priority_queue allocates memory for a pq structure (which you
     // are responsble for defining above. It is
     // completely up to you how this should work.
-    shared_ptr<pq> InitPriorityQueue();
+    shared_ptr<vector<pq>> InitPriorityQueue();
 
     // insert adds the given text to the queue, using the specified
     // priority. If there is a tie, the given string should be placed at
     // the back of the group that it ties with, so all other strings with
     // this priority will be removed first.
-    void Insert(shared_ptr<pq> queue, string text, float priority);
+    void Insert(shared_ptr<vector<pq>> queue, string text, float priority);
 
     // remove will access the highest priority element in the queue,
     // remove it from the queue, and return it. if the queue is empty it
     // should return the empty string.
-    string Remove(shared_ptr<pq> queue);
+    string Remove(shared_ptr<vector<pq>> queue);
 
     // ppek will access the highest priority element in the queue and
     // return it without modifying the queue. if the queue is empty it
     // should return the empty string.
-    string Peek(shared_ptr<pq> queue);    
+    string Peek(shared_ptr<vector<pq>> queue);    
 
 private:
     // you can add add more private member variables and member functions here if you need
+    void heapifyUp(shared_ptr<vector<pq>> queue, int index);
+    void heapifyDown(shared_ptr<vector<pq>> queue, int index);
 };
 
 #endif  // PRIORITYQUEUE_H__
