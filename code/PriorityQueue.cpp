604934d29267512cdbd488e5ab19d07baeadccef
Ryan <rycr3278@colorado.edu>
2023-07-13 21:34:48 +0000

    work in progress

--- code/PriorityQueue.cpp
+++ code/PriorityQueue.cpp
@@ -1,32 +1,79 @@
 #include "PriorityQueue.h"
 
-PriorityQueue::PriorityQueue(){
-    // YOUR CODE HERE
+
+PriorityQueue::PriorityQueue() {
+    // Constructor code here if needed
 }
 
-PriorityQueue::~PriorityQueue(){
-    // You don't need to add anything here
+PriorityQueue::~PriorityQueue() {
+    // Destructor code here if needed
 }
 
-// Implement this after defining struct in .h file
-shared_ptr<pq> PriorityQueue::InitPriorityQueue(){
-    // YOUR CODE HERE
-    return shared_ptr<pq>(NULL);    // replace this line with your own code
+shared_ptr<vector<pq>> PriorityQueue::InitPriorityQueue() {
+    return make_shared<vector<pq>>();
 }
 
-// Implement this second
-void PriorityQueue::Insert(shared_ptr<pq> queue, string text, float priority){
-    // YOUR CODE HERE
+void PriorityQueue::Insert(shared_ptr<vector<pq>> queue, string text, float priority) {
+    if (!queue){
+        return;
+    }
+    pq element;
+    element.text = text;
+    element.priority = priority;
+
+    // Insert the element at the end of the queue
+    queue->push_back(element);
+    heapifyUp(queue, queue->size() - 1);
+    
 }
 
-// Implement this third
-string PriorityQueue::Remove(shared_ptr<pq> queue){
-    // YOUR CODE HERE
-    return "reuslt";    // replace this line with your own code
+string PriorityQueue::Remove(shared_ptr<vector<pq>> queue) {
+    if (queue->empty()) {
+        return "";
+    }
+
+   
+    string result = queue->at(0).text;
+    queue->at(0) = queue->back();
+    queue->pop_back();
+    heapifyDown(queue, 0);
+
+    return result;
 }
 
-// Implement this last
-string PriorityQueue::Peek(shared_ptr<pq> queue){
-    // YOUR CODE HERE
-    return "reuslt";    // replace this line with your own code
+string PriorityQueue::Peek(shared_ptr<vector<pq>> queue) {
+    if (queue->empty()) {
+        return "";
+    }
+
+    return queue->at(0).text;
+}
+
+void PriorityQueue::heapifyUp(shared_ptr<vector<pq>> queue, int index) {
+    int parentIndex = (index - 1) / 2;
+    while (index > 0 && queue->at(parentIndex).priority > queue->at(index).priority) {
+        swap(queue->at(parentIndex), queue->at(index));
+        index = parentIndex;
+        parentIndex = (index - 1) / 2;
+    }
+}
+
+void PriorityQueue::heapifyDown(shared_ptr<vector<pq>> queue, int index) {
+    int size = queue->size();
+    int leftChildIndex = 2 * index + 1;
+    int rightChildIndex = 2 * index + 2;
+    int smallestIndex = index;
+
+    if (leftChildIndex < size && queue->at(leftChildIndex).priority < queue->at(smallestIndex).priority) {
+        smallestIndex = leftChildIndex;
+    }
+
+    if (rightChildIndex < size && queue->at(rightChildIndex).priority < queue->at(smallestIndex).priority) {
+        smallestIndex = rightChildIndex;
+    }
+
+    if (smallestIndex != index) {
+        swap(queue->at(index), queue->at(smallestIndex));
+        heapifyDown(queue, smallestIndex);
+    }
 }
